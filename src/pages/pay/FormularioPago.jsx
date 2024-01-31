637db10fe886dd20f09caf30bc8bc29212db0cc3
Benjamin Morales <benjamin.morales3@mail.udp.cl>
2024-01-31 13:06:38 -0300

    avance

--- src/pages/pay/FormularioPago.jsx
+++ src/pages/pay/FormularioPago.jsx
@@ -1,12 +1,6 @@
-import { useState } from "react"
+import "./styleform.css"
 
-const FormularioPago = ({ title, question, precio, state, changeState }) => {
-    const [temaC, setTemaConsulta] = useState("");
-
-    const handleTemaConsultaChange = (event) => {
-        setTemaConsulta(event.target.checked ? 'tienehijos' : '');
-    };
-
+const FormularioPago = ({ title, question, precio, state, changeState, tipo, logeado }) => {
     return (
         <>
             <form className="teleconsulta_pay">
@@ -14,13 +8,129 @@ const FormularioPago = ({ title, question, precio, state, changeState }) => {
                     <h3 className="h3_teleconsulta">{title}</h3>
                     <b onClick={changeState}>&times;</b>
                 </div>
+
+                {!logeado && (
+                    <>
+                        <div className="input__container">
+                            <label htmlFor="name">Nombre madre</label>
+                            <input type="text" id="name" name="name" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="lastname">Apellido madre</label>
+                            <input type="text" id="lastname" name="lastname" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="age">Edad madre</label>
+                            <input type="text" id="age" name="age" required />
+                        </div>
+                    </>
+                )}
+
+                <div className="input__container">
+                    <label htmlFor="email">Correo madre</label>
+                    <input type="email" name="email" id="email" required />
+                </div>
+
+                {tipo === "embarazo" &&
+                    <>
+                        <div className="input__container">
+                            <label htmlFor="mombirth_date">Fecha de nacimiento de la madre</label>
+                            <input type="date" name="mombirth_date" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="childbirth_date">Fecha estimada del parto</label>
+                            <input type="date" name="childbirth_date" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="birthtype">Tipo de embarazo</label>
+                            <select id="birthtype" name="birthtype" required>
+                                <option value="simple">Simple</option>
+                                <option value="multiple">Multiple</option>
+                            </select>
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="mom_height">Estatura de la madre en cm (opcional)</label>
+                            <input type="text" id="mom_height" name="mom_height" />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="mom_weight">Peso de la madre en kg (opcional)</label>
+                            <input type="text" id="mom_weight" name="mom_weight" />
+                        </div>
+                        <div className="input__container-save">
+                            <input type="checkbox" id="pregnant_before" name="pregnant_before" />
+                            <label htmlFor="pregnant_before">He estado embarazada antes</label>
+                        </div>
+                    </>
+                }
+
+                {tipo === "bebe" &&
+                    <>
+                        <div className="input__container">
+                            <label htmlFor="baby_name">Nombre del bebé</label>
+                            <input type="text" id="baby_name" name="baby_name"/>
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="babybirth_date">Fecha de nacimiento del bebé</label>
+                            <input type="date" name="babybirth_date" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="birthtypebaby">Tipo de parto</label>
+                            <select id="birthtypebaby" name="birthtypebaby" required>
+                                <option value="cesarea">Cesárea</option>
+                                <option value="natural">Natural</option>
+                            </select>
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="baby_height">Estatura del bebé en cm (opcional)</label>
+                            <input type="text" id="baby_height" name="baby_height" />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="baby_weight">Peso del bebé en kg (opcional)</label>
+                            <input type="text" id="baby_weight" name="baby_weight" />
+                        </div>
+                    </>
+                }
+
+                {tipo === "postparto" &&
+                    <>
+                        <div className="input__container">
+                            <label htmlFor="lastbirth">Fecha último parto</label>
+                            <input type="date" id="lastbirth" name="lastbirth" required />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="birthtypebaby">Tipo de parto</label>
+                            <select id="birthtypebaby" name="birthtypebaby" required>
+                                <option value="cesarea">Cesárea</option>
+                                <option value="natural">Natural</option>
+                            </select>
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="baby_height">Estatura del bebé en cm (opcional)</label>
+                            <input type="text" id="baby_height" name="baby_height" />
+                        </div>
+                        <div className="input__container">
+                            <label htmlFor="baby_weight">Peso del bebé en kg (opcional)</label>
+                            <input type="text" id="baby_weight" name="baby_weight" />
+                        </div>
+                        <div className="input__container-save">
+                            <input type="checkbox" id="pregnant_before" name="pregnant_before" />
+                            <label htmlFor="pregnant_before">He estado embarazada antes</label>
+                        </div>
+                    </>
+                }
+
+
                 {question &&
                     (<div className="contenedorpagar__pregunta">
                         <label htmlFor="question">¿Cuál es su pregunta?</label>
                         <input type="text" name="question" id="question" />
                     </div>
-                )}
+                    )}
 
+                <div className="input__container-save">
+                    <input type="checkbox" name="save_data" />
+                    <p >Autorizo que guarden mis datos para futuras consultas.</p>
+                </div>
                 <div className="pagar__buton-container">
                     <button type="submit">Ir a pagar ${precio} US</button>
                 </div>
--- src/pages/pay/Pay.jsx
+++ src/pages/pay/Pay.jsx
@@ -1,168 +1,292 @@
 import { useState } from "react"
 import Footer from "../../components/Footer"
-import Navbar from "../../components/Navbar"
 import images from "../../assets/sintoms"
 import { FaCheckCircle } from "react-icons/fa"
 import FormularioPago from "./FormularioPago"
+import Login from "../../components/Login"
 import { SintomasMadre, SintomasBebe, SintomasPostparto } from './Sintomas'
 import "../pages.css"
 
+const Aviso = ({texto}) => {
+  return(
+    <div className="aviso__texto">
+      <p>{texto}</p>
+    </div>
+  )
+}
+
 const Pay = () => {
+  const [select, setSelect] = useState(false);
+  const [logeado, setLogeado] = useState(true);
+  const [form1, setForm1] = useState(true);
+  const [form2, setForm2] = useState(false);
+  const [form3, setForm3] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [error, setError] = useState(false);
+
+  const changeSelected = () => {
+    setSelect(true);
+  }
+
+  const changeStateErrorTrue = () => {
+    setError(true);
+  }
+
+  const changeStateErrorFalse = () => {
+    setError(false);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evitar que el formulario se envíe automáticamente y recargue la página
+    
+    if (form1) {
+      etapa1();
+    } else if (form2) {
+      etapa2();
+    } // Añade más lógica según tus necesidades
+  };
+
+  const closeLogin = () => {
+    setLogin(false);
+  }
+
+
+  const etapa1 = (e) => {
+    if (temaConsulta === "") {
+      changeStateErrorTrue();
+    }else {
+      changeStateErrorFalse();
+      setForm1(false);
+      setForm2(true);
+    }
+  }
+
+  const volveretapa1 = () => {   
+    changeStateErrorFalse(); 
+    setForm1(true);
+    setForm2(false);
+  }
+
+  const etapa2 = (e) => {
+    changeStateErrorFalse();
+    setForm2(false);
+    setForm3(true);
+  }
+
+  const volveretapa2 = () => {
+    changeStateErrorFalse();
+    setForm2(true);
+    setForm3(false);
+  }
+
+
   const [temaConsulta, setTemaConsulta] = useState("");
 
   const handleTemaConsultaChange = (event) => {
     setTemaConsulta(event.target.value);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleImageClick1 = () => {
+    setTemaConsulta('embarazo');
+  };
 
-    if (temaConsulta === "") {
-      alert("Debes seleccionar una opción antes de enviar el formulario.");
-      return;
-    }
+  const handleImageClick2 = () => {
+    setTemaConsulta('bebe');
+  };
+
+  const handleImageClick3 = () => {
+    setTemaConsulta('postparto');
   };
 
   const [state1, setState1] = useState(false);
+  const [state2, setState2] = useState(false);
+
+  //abrir formulario, es necesario revisar el login antes
   const changeState1 = () => {
-    setState1(!state1);
+    if(logeado){
+      setState1(!state1)
+    }else{
+      setLogin(true)
+    }
   };
 
-  const [state2, setState2] = useState(false);
   const changeState2 = () => {
-    setState2(!state2);
+    if(logeado){
+      setState2(!state2)
+    }else{
+      setLogin(true)
+    }
   };
 
   return (
     <main className="begin__main">
-      {state1 && <FormularioPago precio="3" title="Pregunta" question={true} state={state1} changeState={changeState1}/>}
-      {state2 && <FormularioPago precio="20" title="Teleconsulta" question={false} state={state2} changeState={changeState2}/>}
+      {state1 && <FormularioPago precio="3" title="Pregunta para la consulta" question={true} state={state1} changeState={changeState1} logeado={logeado} tipo={temaConsulta}/>}
+      {state2 && <FormularioPago precio="20" title="Teleconsulta" question={false} state={state2} changeState={changeState2} logeado={logeado} tipo={temaConsulta} />}
+      
+      <form className="begin__form-container" onSubmit={handleSubmit}>
 
-      <form onSubmit={handleSubmit} className="begin__form-container">
         <div className="begin__logo-container">
           <div>
-          <h5 style={{fontSize: "3rem"}}>Pregúntale al Experto</h5>
-          <p style={{textAlign: "center", marginTop: "1rem"}}>Realiza tu teleconsulta rápidamente</p>
+            <h5 style={{ fontSize: "3rem" }}>Pregúntale al Experto</h5>
+            <p style={{ textAlign: "center", marginTop: "1rem" }}>Realiza tu teleconsulta rápidamente</p>
           </div>
         </div>
-        <div className="begin__form-container-1">
-          <div className="begin__form uno">
-            <article className="begin__form--content">
-            <h2 className="titulo">Escoge el tipo de consulta</h2>
-            </article>
 
-          <div className="contenedor-imagenes">
-          <div className="sintomas_container-content1">
-              <label htmlFor="opcion1">Embarazo</label>
-              <div className="begin__form-image">
-                <img src={images.s1} alt="sintoma 1" />
+        {
+          form1 && (
+            <div className="begin__form-container-1">
+              <div className="begin__form uno">
+
+                <article className="begin__form--content">
+                  <h2 className="titulo">Escoge el tipo de consulta</h2>
+                </article>
+
+                {error && (
+                  <Aviso texto="Por favor, ingresa una opción para continuar."/>
+                )}
+
+                <div className="contenedor-imagenes">
+                  <div className="sintomas_container-content1">
+                    <label htmlFor="opcion1">Embarazo</label>
+                    <div className="begin__form-image">
+                      <img style={{cursor: "pointer"}} onClick={handleImageClick1} src={images.s1} alt="sintoma 1" />
+                    </div>
+                    <input
+                      type="radio"
+                      id="opcion1"
+                      name="opciones"
+                      value="embarazo"
+                      checked={temaConsulta === "embarazo"}
+                      onChange={handleTemaConsultaChange}
+                    />
+                  </div>
+
+                  <div className="sintomas_container-content1">
+                    <label htmlFor="opcion2">Bebé</label>
+                    <div className="begin__form-image">
+                      <img style={{cursor: "pointer"}} onClick={handleImageClick2} src={images.s2} alt="sintoma 2" />
+                    </div>
+                    <input
+                      type="radio"
+                      id="opcion2"
+                      name="opciones"
+                      value="bebe"
+                      checked={temaConsulta === "bebe"}
+                      onChange={handleTemaConsultaChange}
+                    />
+                  </div>
+
+                  <div className="sintomas_container-content1">
+                    <label htmlFor="opcion3">Mamá postparto</label>
+                    <div className="begin__form-image">
+                      <img style={{cursor: "pointer"}} onClick={handleImageClick3} src={images.s3} alt="sintoma 3" />
+                    </div>
+                    <input
+                      type="radio"
+                      id="opcion3"
+                      name="opciones"
+                      value="postparto"
+                      checked={temaConsulta === "postparto"}
+                      onChange={handleTemaConsultaChange}
+                    />
+                  </div>
+                </div>
               </div>
-              <input
-                type="radio"
-                id="opcion1"
-                name="opciones"
-                value="embarazo"
-                checked={temaConsulta === "embarazo"}
-                onChange={handleTemaConsultaChange}
-              />
+              <div className="button-next">
+                <button onClick={etapa1}>Siguiente</button>
+              </div>
             </div>
+          )
+        }
 
-            <div className="sintomas_container-content1">
-              <label htmlFor="opcion2">Bebé</label>
-              <div className="begin__form-image">
-                <img src={images.s2} alt="sintoma 2" />
-              </div>
-              <input
-                type="radio"
-                id="opcion2"
-                name="opciones"
-                value="bebe"
-                checked={temaConsulta === "bebe"}
-                onChange={handleTemaConsultaChange}
-              />
-            </div>
-
-            <div className="sintomas_container-content1">
-              <label htmlFor="opcion3">Mamá postparto</label>
-              <div className="begin__form-image">
-                <img src={images.s3} alt="sintoma 3" />
-              </div>
-              <input
-                type="radio"
-                id="opcion3"
-                name="opciones"
-                value="postparto"
-                checked={temaConsulta === "postparto"}
-                onChange={handleTemaConsultaChange}
-              />
-            </div>
-          </div>
-          </div>
-
-          <div className="dos"> 
-            <article className="begin__form--content">
-              <div>
-              <h2>¿Qué sintomas presentas?</h2>
-              </div>
-            </article>
+        {
+          form2 && (
+            <div className="dos">
+              <article className="begin__form--content">
+                <div>
+                  <h2>¿Qué sintomas presentas?</h2>
+                </div>
+              </article>
+              {error && (
+                  <Aviso texto="Por favor, ingresa una opción para continuar."/>
+                )}
               {temaConsulta === "embarazo" && <SintomasMadre />}
               {temaConsulta === "bebe" && <SintomasBebe />}
               {temaConsulta === "postparto" && <SintomasPostparto />}
-          </div>
-        </div>
-
-        <div className="begin__form-2">
-          <div>
-            <h2 style={{textAlign: "center", fontSize: "2.3rem"}}>Tipo de consulta</h2>
-          </div>
-
-          <div className="option__container">
-            <div className="begin__form-option">
-              <h3>Pregúntale al Doctor</h3>
-              <p>
-                <FaCheckCircle className="circle_check" /> Respuesta en menos de 3 horas
-              </p>
-              <p>
-                <FaCheckCircle className="circle_check" /> Médicos especialistas
-              </p>
-              <div className="begin__final-buton">
-                <button onClick={changeState1} type="button">
-                  Realizar pregunta por $3 US
-                </button>
-                <div className="cuppon-container">
-                  <p>Tengo un código de invitación</p>
-                  <input type="text" />
-                </div>
-                <button onClick={changeState2} type="button">
-                  Usar código
-                </button>
+              <div className="button-next">
+                <button onClick={etapa2}>Siguiente</button>
+                <button onClick={volveretapa1}>Volver</button>
               </div>
             </div>
-            <div className="begin__form-option">
-              <h3>Teleconsulta</h3>
-              <p>
-                <FaCheckCircle className="circle_check" /> Teleconsulta de 15 minutos.
-              </p>
-              <p>
-                <FaCheckCircle className="circle_check" /> Expertos en el área para responder sus dudas.
-              </p>
-              <div className="begin__final-buton">
-                <button onClick={changeState2} type="button">
-                  Realizar teleconsulta por $20 US
-                </button>
-                <div className="cuppon-container">
-                  <p>Tengo un código de invitación</p>
-                  <input type="text" />
+          )
+        }
+
+        {
+          form3 && (
+            <div className="begin__form-2">
+              <div>
+                <h2 style={{ textAlign: "center", fontSize: "2.3rem" }}>Tipo de consulta</h2>
+              </div>
+
+              <div className="option__container">
+                <div className="begin__form-option">
+                  <h3>Pregúntale al Doctor</h3>
+                  <p>
+                    <FaCheckCircle className="circle_check" /> Respuesta en menos de 3 horas
+                  </p>
+                  <p>
+                    <FaCheckCircle className="circle_check" /> Médicos especialistas
+                  </p>
+                  <div className="begin__final-buton">
+                    <button onClick={changeState1} type="button">
+                      Realizar pregunta por $3 US
+                    </button>
+                    <div className="cuppon-container">
+                      <p>Tengo un código de invitación</p>
+                      <input type="text" />
+                    </div>
+                    <button onClick={changeState1} type="button">
+                      Usar código
+                    </button>
+                  </div>
                 </div>
-                <button onClick={changeState2} type="button">
-                  Usar código
-                </button>
+
+                <div className="begin__form-option">
+                  <h3>Teleconsulta</h3>
+                  <p>
+                    <FaCheckCircle className="circle_check" /> Teleconsulta de 15 minutos.
+                  </p>
+                  <p>
+                    <FaCheckCircle className="circle_check" /> Expertos en el área para responder sus dudas.
+                  </p>
+                  <div className="begin__final-buton">
+                    <button onClick={changeState2} type="button">
+                      Realizar teleconsulta por $20 US
+                    </button>
+                    <div className="cuppon-container">
+                      <p>Tengo un código de invitación</p>
+                      <input type="text" />
+                    </div>
+                    <button onClick={changeState2} type="button">
+                      Usar código
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="button-next">
+                <button onClick={volveretapa2}>Volver</button>
               </div>
             </div>
-          </div>
-        </div>
+          )
+        }
+        {
+          login && (
+            <Login state={login} changeState={closeLogin}/>
+          )
+        }
       </form>
-      <Footer estilo="pay__footer"/>
+
+      <Footer estilo="pay__footer" />
     </main>
   );
 };
